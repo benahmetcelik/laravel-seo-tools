74748b96aa380717cef51679eae2060010c8309d
webkedi <ben4hmetcelik@gmail.com>
2022-09-03 01:03:27 +0300

    Updated

--- src/SeoServiceProvider.php
+++ src/SeoServiceProvider.php
@@ -25,10 +25,10 @@ class SeoServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'routes');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'seo');
         $this->publishes([
-            __DIR__ . '/../config/routes.php' => config_path('routes.php'),
-            __DIR__ . '/../resources/views' => resource_path('views/vendor/routes'),
+            __DIR__ . '/../config/seo.php' => config_path('seo.php'),
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/seo'),
         ]);
 
 
